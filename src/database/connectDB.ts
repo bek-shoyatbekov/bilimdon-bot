b715a51935a7a3e57e3f55af0178ecb468f53ed7
bek <shoyatbekov03032003@gmail.com>
2024-02-02 20:58:56 +0500

    mongodb connection apiVersion removed

--- src/database/connectDB.ts
+++ src/database/connectDB.ts
@@ -1,15 +1,9 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { MongoClient } from "mongodb";
 
 import config from "../config";
 import { logger } from "../utils/log/logger";
 
-export const client = new MongoClient(config.mongoURI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+export const client = new MongoClient(config.mongoURI);
 
 export const connectDB = async () => {
   try {
